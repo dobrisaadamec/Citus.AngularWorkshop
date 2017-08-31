8b8f6d53690a9c5d7fbfc65e6b0dd96d197a4da1
DESKTOP-KO626ME\dadamec <dadamec@gmail.com>
2017-09-01 00:35:04 +0200

    components and binding

--- src/ClientApp/app/app.module.shared.ts
+++ src/ClientApp/app/app.module.shared.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { ProductappComponent } from './components/productApp/productapp.component';
+import { ProductListComponent } from './components/productApp/product-list.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +18,9 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        ProductappComponent,
+        ProductListComponent
     ],
     imports: [
         CommonModule,
@@ -27,6 +31,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'product-app', component: ProductappComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ src/ClientApp/app/components/productApp/productapp.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'product-app',
+    template: `<h1>This is our Angular playground!</h1>
+    <pm-products></pm-products>`
+})
+export class ProductappComponent {
+}
